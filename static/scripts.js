0732bf97af605294bebddb2ad542c3c06b9877a0
itsespo <157550237+itsespo@users.noreply.github.com>
2024-07-11 23:28:48 -0400

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -1,4 +1,4 @@
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    alert('Thank you for contacting us!');
+    alert('Thank you for contacting us! We will get back to you shortly.');
 });
